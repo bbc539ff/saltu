69c32624675e444f9d3de16f8d4e5e668dcf9728
ffbbc539 <lilracaz@outlook.com>
2019-12-23 21:37:59 +0800

    fix trend and left containner

--- src/components/RightView.vue
+++ src/components/RightView.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="right-notification">
-    <i class="el-icon-message-solid left-icon">通知</i>
-    <div class="notification-content border">
-      <div>Hello Wellcome!</div>
+  <div>
+    <div class="right-notification">
+      <i class="el-icon-message-solid left-icon">通知</i>
+      <div class="notification-content border">
+        <div>Hello Wellcome!</div>
+      </div>
+    </div>
+    <div class="right-notification">
+      <i class="el-icon-data-line left-icon">热门标签</i>
+      <div class="notification-content border">
+        <div>
+          <el-row>
+            <el-col :span="21">
+              <span>#Hello Wellcome!</span>
+            </el-col>
+            <el-col :span="3">
+              <el-button type="primary" icon="el-icon-edit" size="small"></el-button>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
     </div>
   </div>
 </template>
